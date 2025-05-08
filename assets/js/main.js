0e417a9517fb1432f7985903976266c88e2a1fb7
Eunbin Cha <eric9564@gmail.com>
2025-05-08 17:43:45 +0900

    New Version
    New Version

---
+++ assets/js/main.js
@@ -0,0 +1,34 @@
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    effect: 'fade',
+    fadeEffect: { crossFade: true },
+    speed: 800,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    on: {
+        slideChange: function () {
+            const texts = document.querySelectorAll('.slide-text');
+            
+            texts.forEach(text => {
+                text.style.display = "none";
+                text.classList.remove('active');
+            });
+
+            const activeText = texts[this.activeIndex];
+            activeText.style.display = "block";
+
+            void activeText.offsetWidth; 
+
+            activeText.classList.add('active');
+        },
+
+        init: function () {
+            const firstText = document.querySelector('.slide-text');
+            firstText.style.display = "block";
+            firstText.classList.add('active');
+        }
+    }
+});
+
